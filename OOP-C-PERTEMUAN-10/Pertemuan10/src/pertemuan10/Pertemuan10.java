eb077c58e90c2ee796aed33af4647c765300405d
ardinajandrilia <ardinajandrilia4118@gmail.com>
2025-05-24 14:43:30 +0800

    Add files via upload

---
+++ OOP-C-PERTEMUAN-10/Pertemuan10/src/pertemuan10/Pertemuan10.java
@@ -0,0 +1,18 @@
+
+package pertemuan10;
+
+/**
+ *
+ * @author ardina jandrilia
+ * TGL 24 MEI 2025
+ */
+public class Pertemuan10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
